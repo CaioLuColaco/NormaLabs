fd0ac02c6156656e310a5e0416ee3199109c3e8f
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-12-01 15:33:03 -0300

    added a scroll function to buttons of top-menu

--- js/scripts.js
+++ js/scripts.js
@@ -145,4 +145,40 @@ $(document).ready(function() {
         }
     }
 
+
+    // Scroll das seções:
+    let navBtn = $('.nav-item');
+
+    let beginSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let projectsSection = $('#services-area');
+    let teamsSection = $('#team-area');
+    let budgetSection = $('#news-area');
+    let contactSection = $('#call-area');
+    
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+        let btnId = $(this).attr('id');
+
+        if(btnId == 'home-menu') {
+            scrollTo = beginSection;
+        } else if(btnId == 'about-menu') {
+            scrollTo = aboutSection;
+        } else if(btnId == 'services-menu') {
+            scrollTo = projectsSection;
+        } else if(btnId == 'team-menu') {
+            scrollTo = teamsSection;
+        } else if(btnId == 'news-menu') {
+            scrollTo = budgetSection;
+        } else if(btnId == 'call-menu') {
+            scrollTo = contactSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500);
+    });
+
+    
 });
