be156c6986887b70821a3954880e796d3bb498d8
Caio Lucena Colaço <caio.colaco@normatel.com.br>
2021-12-09 11:17:31 -0300

    adjusted the image of carousel and make a changes of eric

--- views/public/scripts.js
+++ views/public/scripts.js
@@ -1,109 +1,109 @@
 $(document).ready(function() {
-    let containerA = document.getElementById("circleA");
+    // let containerA = document.getElementById("circleA");
 
-    let circleA = new ProgressBar.Circle(containerA, {
+    // let circleA = new ProgressBar.Circle(containerA, {
 
-        color: '#F48C06',
-        strokeWidth: 8,
-        duration: 1400,
-        from: { color: '#AAA' },
-        to: { color: '#F48C06'},
+    //     color: '#F48C06',
+    //     strokeWidth: 8,
+    //     duration: 1400,
+    //     from: { color: '#AAA' },
+    //     to: { color: '#F48C06'},
 
-        step: function(state, circle) {
-            circle.path.setAttribute('stroke', state.color);
+    //     step: function(state, circle) {
+    //         circle.path.setAttribute('stroke', state.color);
 
-            let value = Math.round(circle.value() * 60);
+    //         let value = Math.round(circle.value() * 60);
 
-            circle.setText(value);
+    //         circle.setText(value);
 
-        }
+    //     }
 
-    });
+    // });
 
-    let containerB = document.getElementById("circleB");
+    // let containerB = document.getElementById("circleB");
 
-    let circleB = new ProgressBar.Circle(containerB, {
+    // let circleB = new ProgressBar.Circle(containerB, {
 
-        color: '#F48C06',
-        strokeWidth: 8,
-        duration: 1800,
-        from: { color: '#AAA' },
-        to: { color: '#F48C06'},
+    //     color: '#F48C06',
+    //     strokeWidth: 8,
+    //     duration: 1800,
+    //     from: { color: '#AAA' },
+    //     to: { color: '#F48C06'},
 
-        step: function(state, circle) {
-            circle.path.setAttribute('stroke', state.color);
+    //     step: function(state, circle) {
+    //         circle.path.setAttribute('stroke', state.color);
 
-            let value = Math.round(circle.value() * 254);
+    //         let value = Math.round(circle.value() * 254);
 
-            circle.setText(value);
+    //         circle.setText(value);
 
-        }
+    //     }
 
-    });
+    // });
 
-    let containerC = document.getElementById("circleC");
+    // let containerC = document.getElementById("circleC");
 
-    let circleC = new ProgressBar.Circle(containerC, {
+    // let circleC = new ProgressBar.Circle(containerC, {
 
-        color: '#F48C06',
-        strokeWidth: 8,
-        duration: 2200,
-        from: { color: '#AAA' },
-        to: { color: '#F48C06'},
+    //     color: '#F48C06',
+    //     strokeWidth: 8,
+    //     duration: 2200,
+    //     from: { color: '#AAA' },
+    //     to: { color: '#F48C06'},
 
-        step: function(state, circle) {
-            circle.path.setAttribute('stroke', state.color);
+    //     step: function(state, circle) {
+    //         circle.path.setAttribute('stroke', state.color);
 
-            let value = Math.round(circle.value() * 14);
+    //         let value = Math.round(circle.value() * 14);
 
-            circle.setText(value);
+    //         circle.setText(value);
 
-        }
+    //     }
 
-    });
+    // });
 
-    let containerD = document.getElementById("circleD");
+    // let containerD = document.getElementById("circleD");
 
-    let circleD = new ProgressBar.Circle(containerD, {
+    // let circleD = new ProgressBar.Circle(containerD, {
 
-        color: '#F48C06',
-        strokeWidth: 8,
-        duration: 2400,
-        from: { color: '#AAA' },
-        to: { color: '#F48C06'},
+    //     color: '#F48C06',
+    //     strokeWidth: 8,
+    //     duration: 2400,
+    //     from: { color: '#AAA' },
+    //     to: { color: '#F48C06'},
 
-        step: function(state, circle) {
-            circle.path.setAttribute('stroke', state.color);
+    //     step: function(state, circle) {
+    //         circle.path.setAttribute('stroke', state.color);
 
-            let value = Math.round(circle.value() * 8142);
+    //         let value = Math.round(circle.value() * 8142);
 
-            circle.setText(value);
+    //         circle.setText(value);
 
-        }
+    //     }
 
-    });
+    // });
 
-    // Iniciando quando chega no elemento
-    let dataAreaOffset = $('#data-area').offset();
-    let stop = 0;
+    // // Iniciando quando chega no elemento
+    // let dataAreaOffset = $('#data-area').offset();
+    // let stop = 0;
 
-    $(window).scroll(function(e) {
+    // $(window).scroll(function(e) {
 
-        let scroll = $(window).scrollTop();
+    //     let scroll = $(window).scrollTop();
 
-        if(scroll > (dataAreaOffset.top - 500) && stop == 0) {
-            circleA.animate(1.0);
-            circleB.animate(1.0);
-            circleC.animate(1.0);
-            circleD.animate(1.0);
+    //     if(scroll > (dataAreaOffset.top - 500) && stop == 0) {
+    //         circleA.animate(1.0);
+    //         circleB.animate(1.0);
+    //         circleC.animate(1.0);
+    //         circleD.animate(1.0);
 
-            stop = 1;
-        }
-    })
+    //         stop = 1;
+    //     }
+    // })
 
     // Parallax
     setTimeout(function() {
-        $('#data-area').parallax({imageSrc : '/pics/paralax4.jpeg'})
+        // $('#data-area').parallax({imageSrc : '/pics/paralax4.jpeg'})
         $('#apply-area').parallax({imageSrc : '/pics/fundoCircuito5.png'})
     }, 250);
 
